19e1a532c126d29530d676f34a1d6782ada3cf90
stellarAlien <saifgaida@gmail.com>
2022-05-20 14:18:21 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
+# Double the number of webservers.
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo touch index.html /var/www/html/
-echo 'dummy text' > /var/www/html/index.html
+echo 'Holberton School' > /var/www/html/index.html
 sudo mkdir /var/www/error
+echo "Ceci n'est pas une page" > /var/www/error/404.html
+sed -i '/server_name _/a location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlw\u4? permanent; }' /etc/nginx/sites-available/default
+sed -i '/server_name _/a error_page 404 /404.html; location = /404.html {root /var/www/error/; internal; }' /etc/nginx/sites-available/default
+sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-#sudo chown $USER:$USER /data/web_static/releases/test/
-echo "dummy text" > /data/web_static/releases/test/index.html
-echo "Ceci n'est pas une page" > /var/www/error/404.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
 sudo service nginx restart
-sudo chown -R ubuntu:ubuntu /data/
